c79630756dcbe933d72650224a1143c495418885
kykwak <kiyoung.kwak@ta9.co.kr>
2024-09-12 15:02:11 +0900

    커밋 테스트

--- src/routes/Card.js
+++ src/routes/Card.js
@@ -4,6 +4,12 @@ function Card(props) {
       <img src={"/shoes" + (props.i + 1) + ".jpg"} width="80%" />
       <h4>{props.shoes.title}</h4>
       <p>{props.shoes.price}</p>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
     </div>
   );
 }
